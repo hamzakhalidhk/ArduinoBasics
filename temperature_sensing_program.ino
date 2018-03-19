38ea26579871d7eaeb62e0fb308d9ee58f7bf683
Hamza Khalid <itshamzakhalidhk@gmail.com>
2018-03-19 07:59:27 -0700

    Rename temperature_sensing.ino to temperature_sensing_program.ino

---
+++ temperature_sensing_program.ino
@@ -0,0 +1,23 @@
+int sensorPin = 0;
+void setup()
+{
+Serial.begin(9600); //Start the serial connection with the computer
+//to view the result open the serial monitor
+}
+void loop() // run over and over again
+{
+//getting the voltage reading from the temperature sensor
+int reading = analogRead(sensorPin);
+// converting that reading to voltage, for 3.3v arduino use 3.3
+float voltage = reading * 5.0;
+voltage /= 1024.0;
+// print out the voltage
+Serial.print(voltage); Serial.println(" volts");
+// now print out the temperature
+float temperatureC = (voltage - 0.5) * 100 ; //converting from 10 mv per degree wit 500 mV offset
+//to degrees ((voltage - 500mV) times 100)
+Serial.print(temperatureC); Serial.println(" degrees C");
+// now convert to Fahrenheit
+float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+Serial.print(temperatureF); Serial.println(" degrees F");
+delay(1000); //waiting a second
